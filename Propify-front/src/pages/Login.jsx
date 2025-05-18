a50fcab473e68a1c478dc3705117dd6fd0b2aa4b
Ezequiel Maciel <emaciel19@gmail.com>
2025-05-18 15:27:59 -0300

    estilos

--- Propify-front/src/pages/Login.jsx
+++ Propify-front/src/pages/Login.jsx
@@ -1,10 +1,14 @@
 import LoginForm from "../components/forms/LoginForm.jsx";
+import cabinImage from "../assets/cabin.png";
+import "../styles/LoginStyle.css";
+
 
 
 const LoginPage = () => {
 
     return (
       <div className="login-container">
+        <img src={cabinImage} alt="Fondo de login" className="background-image" />
         <LoginForm />
       </div>
     );

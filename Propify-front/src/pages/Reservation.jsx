412f88d2c2c198f176b4f123368fea96d68d368d
Ezequiel Maciel <emaciel19@gmail.com>
2025-06-06 15:20:50 -0300

    header

--- Propify-front/src/pages/Reservation.jsx
+++ Propify-front/src/pages/Reservation.jsx
@@ -3,12 +3,14 @@ import bedImage from "../assets/fondoreserva.png";
 import "../styles/LoginStyle.css";
 import ReservationForm from "../components/forms/ReservationForm.jsx";
 import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header.jsx";
 
 
 const ReservationPage = () => {
 
 return (
   <>
+  <Header/>
     <div className="reservation-page">
       <div className="header-image">
         <img src={bedImage} alt="Fondo de reserva" />
